51e87fd1726fca15b462ebdd9e845d0116a6b100
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-12-01 20:58:44 +0100

    create components App.js Hero.js List.js

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './App.scss';
+
+const MyComponent = () => (
+    <header className= {styles.title}>
+        <h2>Things to do</h2>
+        <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"  />
+    </header>
+);
+
+export default MyComponent;
