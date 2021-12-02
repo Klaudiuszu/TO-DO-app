c9e789a9be9c62f4b1b17f5c10af839ca2f16f45
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-12-02 19:49:44 +0100

    add Gero.js component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './App.scss';
+import styles from '../App/App.scss';
 
-const MyComponent = () => (
-    <header className= {styles.title}>
+const Hero = () => (
+    <header className={styles.title}>
         <h2>Things to do</h2>
         <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"  />
     </header>
 );
 
-export default MyComponent;
+export default Hero;
