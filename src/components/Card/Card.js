cb2b407499598a74ede07026bb22797881796bf4
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-12-08 22:34:32 +0100

    modify components and install eslint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import styles from '../Card/Card.scss';
 import PropTypes from 'prop-types';
 
 
 class Card extends React.Component {
 
-    static propTypes = {
-        title:  PropTypes.node.isRequired,
-        description: PropTypes.node,
-        columns: PropTypes.array,
-    }
+  static propTypes = {
+    title:  PropTypes.node.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+  };
 
-    render(){
-        console.log(this.props)
-        const {title} = this.props;
-        return(
+  render(){
 
-        <div>{title}</div>
+    const {title} = this.props;
+    return(
 
-        )
-    }
+      <div>{title}</div>
 
+    );
+  }
 }
 
 export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,52 +5,57 @@ import Creator from '../Creator/Creator';
 import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 import PropTypes from 'prop-types';
-
 class Column extends React.Component {
 
-    state = {
-        cards: this.props.cards || []
+
+  static propTypes = {
+    cards: PropTypes.array,
+    title: PropTypes.string,
+    icon: PropTypes.string,
+  };
+
+  state = {
+    cards: this.props.cards || [],
+  };
+
+  addCard(title){
+    this.setState(state => ({
+      cards: [
+        ...state.cards,
+        {
+          key:state.cards.length ? state.cards[state.cards.length-1].
+            key+1 : 0,
+          title,
+        },
+      ],
     }
+    ));
+  }
 
-    addCard(title){
-        this.setState(state => (
-            {
-                cards: [
-                    ...state.cards,
-                    {
-                        key:state.cards.length ? state.cards[state.cards.length-1].
-                        key+1 : 0,
-                        title,
-                    }
-                ]
-            }
-        ));
-    }
+  render() {
+    const {title, icon} = this.props;
 
-    render() {
-        const {title, icon} = this.props;
+    return (
 
-        return (
-
-        <section className={styles.component}>
-            <h3 className={styles.title}>
-            <span className={styles.icon}>
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
             <Icon name={icon} />
-            </span>
-            {title}
-            </h3>
+          </span>
+          {title}
+        </h3>
 
-            {this.state.cards.map(({key, ...cardProps}) => (
-                <Card key={key} {...cardProps} />
-            ))}
-            <div>
-                <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-            </div>
+        {this.state.cards.map(({key, ...cardProps}) => (
+          <Card key={key} {...cardProps} />
+        ))}
+        <div>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
 
-        </section>
-        
-        )
-    }
+      </section>
+
+    );
+  }
 }
 
 export default Column;
