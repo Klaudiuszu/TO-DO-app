4112cfbc9ab5acc63228923b30d8607e370653b1
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-12-16 22:02:58 +0100

    add react router in project

---
+++ src/components/Info/Info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const Info = () => (
+  <Container>
+    <h2>Info</h2>
+    <p>TMP TEKST</p>
+  </Container>
+);
+
+export default Info;
